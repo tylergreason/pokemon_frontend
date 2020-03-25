810b8caba84a9dc46d2b1a77c23c9a8e8be8f4f4
tylergreason <tyler.greason@protonmail.com>
2020-03-25 09:26:43 -0400

    cards looking good. Moving to presenting them in a grid.

--- src/components/PokemonCardSmall.js
+++ src/components/PokemonCardSmall.js
@@ -1,26 +1,56 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
+import { Card, Label, Image, List, Divider } from 'semantic-ui-react' 
 
 // const PokemonCardSmall = (props) => {
 class PokemonCardSmall extends Component {
 
+
     renderAttributes = (attributes) => {
         return attributes.map(attribute => <label >{attribute.name}/</label>)
+    }
+    renderTypes = (types) => {
+        return <Card.Meta>
+            {types.map(type => {
+                return <Label>{type.name}</Label>
+                }
+                )}
+        </Card.Meta>
+    }
+
+    renderMoves = (moves) => {
+        return <List horizontal> 
+            {moves.map(move => {
+                return <List.Item>
+                    <Label>
+                        {move.name}
+                    </Label>
+                </List.Item> 
+            })}
+        </List>
     }
 
     render (){
         return(
-        <div 
-            style={{border: '5px solid black', width:'25%'}}
+        <Card 
             // onClick={() => this.props.handleClick(this.props.pokemon)}
             onClick={() => this.props.handleClick(this.props.pokemon)}
         >
-            <h3>{this.props.pokemon.name}</h3>
-            <img src={this.props.pokemon.image_url}/>
-            <p>Types: </p>
-            <React.Fragment>{this.renderAttributes(this.props.pokemon.types)}</React.Fragment>
-            <p>Moves: </p>
-            <React.Fragment>{this.renderAttributes(this.props.pokemon.active_moves)}</React.Fragment>
-        </div>
+            <Card.Content>
+            <Card.Header>{this.props.pokemon.name}</Card.Header>
+            <Image src={this.props.pokemon.image_url} size='medium' centered/>
+
+            {this.renderTypes(this.props.pokemon.types)}
+            <Card.Meta>
+
+            {/* <p>Types: </p>
+            <React.Fragment>{this.renderAttributes(this.props.pokemon.types)}</React.Fragment> */}
+            </Card.Meta>
+            <Divider />
+            {this.renderMoves(this.props.pokemon.active_moves)}
+            {/* <p>Moves: </p>
+            <React.Fragment>{this.renderAttributes(this.props.pokemon.active_moves)}</React.Fragment> */}
+            </Card.Content>
+        </Card>
         )
     }
 }
